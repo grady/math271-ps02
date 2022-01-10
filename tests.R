1b0d86d50579ca7c53d470b7ed2c5e10826e2382
Grady Weyenberg <gradysw@hawaii.edu>
2022-01-09 17:57:35 -1000

    first try for instructions

--- tests.R
+++ tests.R
@@ -1,2 +1,42 @@
 ## execute all code in the solution file
-try(knitr::knit(text=readLines('PSxx_solution.Rmd')))
+try(knitr::knit(text=readLines('PS02_solution.Rmd')))
+
+test_that("Data import", {
+  expect_equal(dim(nc), c(800, 13))
+})
+
+test_that("Exercise 3", {
+  expect_s3_class(nc_gained, "ggplot")
+})
+
+test_that("Exercise 5", {
+  expect_s3_class(nc_mage, "ggplot")
+})
+
+test_that("Exercise 7", {
+  expect_s3_class(weight_hist, "ggplot")
+})
+
+test_that("Exercise 8", {
+  expect_s3_class(weight_gender, "ggplot")
+})
+
+test_that("Exercise 9", {
+  expect_s3_class(gained_mature, "ggplot")
+})
+
+test_that("Exercise 10", {
+  expect_s3_class(weeks_habit, "ggplot")
+})
+
+test_that("Exercise 11", {
+  expect_s3_class(fage_plot, "ggplot")
+})
+
+test_that("Exercise 12", {
+  expect_s3_class(weight_whitemom, "ggplot")
+})
+
+test_that("Exercise 13", {
+  expect_s3_class(mage_weeks, "ggplot")
+})

dd3374996aee4598d3a460caa3034d66d624d614
Grady Weyenberg <gradysw@hawaii.edu>
2023-12-15 12:21:17 -1000

    updates to tests.R

--- tests.R
+++ tests.R
@@ -1,42 +1,45 @@
 ## execute all code in the solution file
-try(knitr::knit(text=readLines('PS02_solution.Rmd')))
-
-test_that("Data import", {
-  expect_equal(dim(nc), c(800, 13))
+test_that("<5>Solution file exists and knits",{
+  expect_true(file.exists("solution.Rmd"))
+  suppressWarnings(expect_error(knitr::knit("solution.Rmd", quiet = TRUE, envir=globalenv()), NA))
 })
 
-test_that("Exercise 3", {
-  expect_s3_class(nc_gained, "ggplot")
+test_that("<1>Data import", {
+  expect_error(expect_equal(dim(nc), c(800, 13)), NA)
 })
 
-test_that("Exercise 5", {
-  expect_s3_class(nc_mage, "ggplot")
+test_that("<1>Exercise 3", {
+  expect_error(expect_s3_class(nc_gained, "ggplot"), NA)
 })
 
-test_that("Exercise 7", {
-  expect_s3_class(weight_hist, "ggplot")
+test_that("<1>Exercise 5", {
+  expect_error(expect_s3_class(nc_mage, "ggplot"), NA)
 })
 
-test_that("Exercise 8", {
-  expect_s3_class(weight_gender, "ggplot")
+test_that("<1>Exercise 7", {
+  expect_error(expect_s3_class(weight_hist, "ggplot"), NA)
 })
 
-test_that("Exercise 9", {
-  expect_s3_class(gained_mature, "ggplot")
+test_that("<1>Exercise 8", {
+  expect_error(expect_s3_class(weight_gender, "ggplot"), NA)
 })
 
-test_that("Exercise 10", {
-  expect_s3_class(weeks_habit, "ggplot")
+test_that("<1>Exercise 9", {
+  expect_error(expect_s3_class(gained_mature, "ggplot"), NA)
 })
 
-test_that("Exercise 11", {
-  expect_s3_class(fage_plot, "ggplot")
+test_that("<1>Exercise 10", {
+  expect_error(expect_s3_class(weeks_habit, "ggplot"), NA)
 })
 
-test_that("Exercise 12", {
-  expect_s3_class(weight_whitemom, "ggplot")
+test_that("<1>Exercise 11", {
+  expect_error(expect_s3_class(fage_plot, "ggplot"), NA)
 })
 
-test_that("Exercise 13", {
-  expect_s3_class(mage_weeks, "ggplot")
+test_that("<1>Exercise 12", {
+  expect_error(expect_s3_class(weight_whitemom, "ggplot"), NA)
+})
+
+test_that("<1>Exercise 13", {
+  expect_error(expect_s3_class(mage_weeks, "ggplot"), NA)
 })
